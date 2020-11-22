c261c880be530725c0c2be7eb26ee326edc7832a
B179280-2020 <email_not_provided>
2020-11-22 23:40:10 +0000

    Added codes to scan protein seqs with motifs from PROSITE

--- ass2.py
+++ ass2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import os, sys, shutil
+import os, sys, shutil,re
 import subprocess
 import matplotlib.pyplot as plt
 
+
+taxon_gp = ""
 #ask the user about what protein family and taxonomic group they like
 def getInput():
+	global taxon_gp
 	protein_fam = input("Enter the protein family name\n")
 	taxon_gp = input("Enter the taxonomic group name\n")
 	print("Thanks, you have chosen " + protein_fam + " in " + taxon_gp + "\n")
@@ -20,19 +23,19 @@ def findSeq():
 	seqnumber= subprocess.getoutput("grep -c \">\" seq.fa")
 	seq_number = int(seqnumber)
 	return seq_number
-findSeq()
-print("There are " + str(seq_number) + " sequences in the dataset you have chosen")
+seq_Num = findSeq()
+print("There are " + str(seq_Num) + " sequences in the dataset you have chosen")
 #Test if the the number of the sequences are appropriate and give the user options to decide if they want to continue
 
-def sequence_check(seq_number):
-	while seq_number <= 1 or seq_number >= 1000:
-		print("Sorry,there is no result or over 1000 sequences. Please input again!")
-		if seq_number >= 1 and seq_number <= 1000:
+def sequence_check(seq_Num):
+	while seq_Num <= 1 or seq_Num >= 1000:
+		print("Sorry,there is no result or over 1000 sequences. Please check and input again!")
+		if seq_Num >= 1 and seq_Num <= 1000:
                         break
 		getInput()
-		findSeq()
-		return seq_number
-sequence_check(seq_number)
+		seq_Num = findSeq()
+		return seq_Num
+sequence_check(seq_Num)
 
 #Tell the user the sequence number and ask them if they are willing to continue
 print("There are " + str(seq_number) + " sequences in the dataset you have chosen")
@@ -42,8 +45,8 @@ while choice1 == "N":
 		break
 	print("Thanks, please then input what you want again")
 	getInput()
-	findSeq()
-	print("There are " + str(seq_number) + " sequences in the dataset you have chosen")
+	seq_Num = findSeq()
+	print("There are " + str(seq_Num) + " sequences in the dataset you have chosen")
 	choice1 = input("Do you want to continue?,Y/N\n")
 	
 #find the number of species in this dataset
@@ -58,27 +61,36 @@ def findSpec():
 		spe_num.append(i[l1:l2])
 	spe_n = len(set(spe_num))
 	return spe_n
-findSpec()
+spe_N = findSpec()
 #Tell the user the number of species in this dataset and ask them if you want to continue
-print("There are " + str(spe_n) + " species in this dataset")
+print("There are " + str(spe_N) + " species in this dataset")
 choice2 = input("Do you want to continue with the current dataset? Y/N\n")
 while choice2 == "N":
         if choice2 == "Y":
                 break
         print("Thanks, please then input what you want again")
-        getInput()
-        findSeq()
-	findSpec()
-        print("There are " + str(spe_n) + " species in this dataset")
+	getInput()
+	seq_Num = findSeq()
+	spe_N = findSpec()
+        print("There are " + str(spe_N) + " species in this dataset")
         choice2 = input("Do you want to continue?,Y/N\n")
 
+
 #move on to the main data processing procedure
+#using clustalo to align the data
+clus = "clustalo -i seq.fa > align.fa"
+print(clus)
+subprocess.call(clus,shell=True)
+print("Clustalo has been successfully done!")
+print("The alignment data has been store in the align.fa file")
+
 
 #using clustalo to align the data
 os.system("clustalo -i seq.fa > align.fa")
 print("The alignment data has been store in the align.fa file")
 
 #find a representative sequence for blast analysis
+#find the sequence with the lease number of "-" in the align.fa file
 def _findId():
 	f = open('align.fa','r')
 
@@ -87,7 +99,6 @@ def _findId():
 
 	for item in item_list:
 		_num.append(item.count('-'))
-
 	print('- number is ' + str(_num[_num.index(min(_num))]) + '\n')
 	print('The representative sequence ID is:\n')
 	rep_seq = item_list[_num.index(min(_num))]
@@ -98,16 +109,26 @@ def _findId():
 seq_Id=_findId()
 
 #download the test sequence using the seq_id
-os.system("esearch -db protein -query seq_Id | efetch -db protein -format fasta > test_seq.fa")
+os.system("esearch -db protein -query " + seq_Id + " | efetch -db protein -format fasta > test_seq.fa")
+
 #using this representative sequence for blast
 #make blast data base first
-subprocess.call("makeblstdb -in seq.fa -dbtype prot -out " + taxon_gp)
+mdb = "makeblastdb -in seq.fa -dbtype prot -out " + taxon_gp
+print(mdb)
+subprocess.call(mdb,shell=True)
+
 
 #doing blast using test sequence and data base
-subprocess.call("blastp -db " + taxon_gp + " -query test_seq.fa -outfmt 7 > blastoutput.out")
+blt = "blastp -db " + taxon_gp + " -query test_seq.fa -outfmt 7 > blastoutput.out"
+print(blt)
+subprocess.call(blt,shell=True)
+
 	
 #find the first 250 sequence id of the blastoutput file
-subprocess.call("grep -v \"#\" blastoutput.out | cut -f2 | head -250 > blast250.txt")
+fd = "grep -v \"#\" blastoutput.out | cut -f2 | head -250 > blast250.txt"
+print(fd)
+subprocess.call(fd,shell=True)
+
 
 #using pullseq to download these 250 sequences
 pu = "/localdisk/data/BPSM/Assignment2/pullseq -i seq.fa -n blast250.txt > seq_pull_250.fasta"
@@ -143,3 +164,32 @@ inf = "infoalign -sequence seq_pull_250.fasta -outfile seq250.infoalign"
 print(inf)
 subprocess.call(inf,shell = True)
 print("The infoalign analysis results have been saved in seq250.infoalign file")
+
+
+#PROSITE analysis to find motifs
+blt250 = open("blast250.txt").read().split("\n")
+print(blt250[:-1])
+motif_array = []
+found_motif = open('fnd_motifs.txt',"w")
+found_motif.write("Accession ID\tMotif\n")
+for i in blt250[:-1]:
+        ese = "esearch -db protein -query " + i + " | efetch -db protein -format fasta > int_seq.fa"
+        #print(ese)
+        subprocess.call(ese,shell=True)
+        pam = "patmatmotifs -sequence int_seq.fa -outfile int_seqmotif -full"
+        #print(pam)
+        subprocess.call(pam,shell=True)
+#find if there are any motifs
+        pro_file = open("int_seqmotif").readlines()
+        for line in pro_file:           #through each lines
+                if re.search('#',line):         #skipping comments
+                        next
+                elif re.search('Motif', line):       #searching for line where Motif word used
+                        line.rstrip()                #cutting new line from the end of line, in order to not to get in my match
+                        index = line.find("=")       #motif name is after the sign '=', that's why I want to get index of this sign
+                        motif = line[index+2:]       #getting name of the Motif after '= ' and space, so I add 2
+                        motif_array.append(motif)    #saving found motif names into array
+                        found_motif.write('{0}\t{1}'.format(i,motif))
+
+print("The output of all sequences' scanning for motifs from the PROSITE database are saved in the fnd_motifs.txt file")
+
